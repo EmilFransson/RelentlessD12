cf5479099093dc5e74074acce86da49c080fb53c
Emil Fransson <emil.fransson.1@hotmail.com>
2026-07-31 15:29:30 +0200

    Implemented ExponentialHeightFogRenderProxy

---
+++ Relentless/src/Graphics/RenderProxy/ExponentialHeightFogRenderProxy.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Graphics/RHI/Texture.h"
+
+namespace Relentless
+{
+	struct ExponentialHeightFogRenderProxy
+	{
+		Vector3 InScatteringColor				= Vector3::Zero;
+		Vector3 InScatteringTextureColorTint	= Vector3::One;
+		float DensityLayer0						= 0.0f;
+		float DensityLayer1						= 0.0f;
+		float HeightFallOffLayer0				= 0.0f;
+		float HeightFallOffLayer1				= 0.0f;
+		float StartDistanceLayer0				= 0.0f;
+		float StartDistanceLayer1				= 0.0f;
+		float EndDistanceLayer0					= 0.0f;
+		float EndDistanceLayer1					= 0.0f;
+		float HeightOffsetLayer0				= 0.0f;
+		float HeightOffsetLayer1				= 0.0f;
+		float MaxOpacity						= 0.0f;
+		bool UseUniformInscatter				= true;
+		Ref<Texture> InScatterTexture			= nullptr;
+	};
+}
